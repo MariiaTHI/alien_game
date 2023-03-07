1bda54ecf4332ac1694ea90540b25a7e5a336018
Maria <mat8815@thi.de>
2023-03-07 17:57:15 +0100

    added import ABC

---
+++ Player.py
@@ -0,0 +1,24 @@
+# -----------------------------
+#            TEAM 2
+# -----------------------------
+from abc import ABC, abstractmethod
+
+class Player(ABC):
+    """
+    Abstract Player class
+    """
+
+    def __init__(self, cols):
+        """
+        Constructor
+        :param cols: the available columns of the play field
+        """
+        pass
+
+    @abstractmethod
+    def shoot(self):
+        """
+        Abstract method to shoot at a selected column
+        :return: the column to shoot at
+        """
+        pass
